622bd4ddef478e927c2f0c0680f7403a5eec367d
duraisas <duraisas@yahoo.com>
2025-01-23 15:33:34 -0500

    Api Call added streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,10 @@ if ingredients_list:
     #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit the Order')
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success("Your smoothie is ordered "+name_on_order,icon="✅")
